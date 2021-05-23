ea3fddab720cf0b0a36f71b8c9cb9524f060034f
evs-dev <evansmeath@gmail.com>
2021-05-23 14:14:45 +0100

    Change tabs to 4 spaces

--- src/me/EvsDev/EnderDragonTweaks/ConfigManager.java
+++ src/me/EvsDev/EnderDragonTweaks/ConfigManager.java
@@ -6,49 +6,49 @@ import org.bukkit.util.Vector;
 
 public class ConfigManager {
 
-	public static final String entry_enabled = "enabled";
-	public static final String entry_delay = "delay";
-	public static final String entry_playerDistanceFromOrigin = "max-player-distance-from-end-centre";
-	public static final String entry_xpMode = "xp-mode";
-	public static final String entry_xpPerPlayer = "xp-per-player";
-	public static final String entry_orbCountPerPlayer = "orb-count-per-player";
-	public static final String entry_defeatMessage = "defeat-announcement-message";
-	public static final String entry_enableXP = "enable-xp-drop";
-	public static final String entry_enableDecorationOrbs = "enable-decoration-orbs";
-	public static final String entry_enableEggRespawn = "enable-egg-respawn";
-	public static final String entry_enableDefeatAnnouncement = "enable-defeat-announcement";
+    public static final String entry_enabled = "enabled";
+    public static final String entry_delay = "delay";
+    public static final String entry_playerDistanceFromOrigin = "max-player-distance-from-end-centre";
+    public static final String entry_xpMode = "xp-mode";
+    public static final String entry_xpPerPlayer = "xp-per-player";
+    public static final String entry_orbCountPerPlayer = "orb-count-per-player";
+    public static final String entry_defeatMessage = "defeat-announcement-message";
+    public static final String entry_enableXP = "enable-xp-drop";
+    public static final String entry_enableDecorationOrbs = "enable-decoration-orbs";
+    public static final String entry_enableEggRespawn = "enable-egg-respawn";
+    public static final String entry_enableDefeatAnnouncement = "enable-defeat-announcement";
 
-	public static final String entry_eggPositionSection = "egg-position.";
-	public static final String entry_overrideEggY = entry_eggPositionSection + "override-y";
+    public static final String entry_eggPositionSection = "egg-position.";
+    public static final String entry_overrideEggY = entry_eggPositionSection + "override-y";
 
-	private final FileConfiguration config;
+    private final FileConfiguration config;
 
-	public ConfigManager(Plugin plugin) {
-		this.config = plugin.getConfig();
-		plugin.saveDefaultConfig();
-		if (this.getInt("xp-points-per-player") != 0) {
-		    Util.logWarning("Your EnderDragonTweaks config is old! Delete/rename it and restart the server to generate a new one.");
-		}
-	}
+    public ConfigManager(Plugin plugin) {
+        this.config = plugin.getConfig();
+        plugin.saveDefaultConfig();
+        if (this.getInt("xp-points-per-player") != 0) {
+            Util.logWarning("Your EnderDragonTweaks config is old! Delete/rename it and restart the server to generate a new one.");
+        }
+    }
 
-	public boolean getBoolean(String name) {
-		return this.config.getBoolean(name);
-	}
+    public boolean getBoolean(String name) {
+        return this.config.getBoolean(name);
+    }
 
-	public int getInt(String name) {
-		return this.config.getInt(name);
-	}
+    public int getInt(String name) {
+        return this.config.getInt(name);
+    }
 
-	public String getString(String name) {
-		return this.config.getString(name);
-	}
+    public String getString(String name) {
+        return this.config.getString(name);
+    }
 
-	public Vector getEggLocationAsVector() {
-		return new Vector(
-			this.getInt(entry_eggPositionSection + "x"),
-			this.getInt(entry_eggPositionSection + "y"),
-			this.getInt(entry_eggPositionSection + "z")
-		);
-	}
+    public Vector getEggLocationAsVector() {
+        return new Vector(
+            this.getInt(entry_eggPositionSection + "x"),
+            this.getInt(entry_eggPositionSection + "y"),
+            this.getInt(entry_eggPositionSection + "z")
+        );
+    }
 
 }
--- src/me/EvsDev/EnderDragonTweaks/EnderDragonDeathListener.java
+++ src/me/EvsDev/EnderDragonTweaks/EnderDragonDeathListener.java
@@ -19,140 +19,140 @@ import org.bukkit.util.Vector;
 
 public class EnderDragonDeathListener implements Listener {
 
-	private static int delayTicks = 80;
-	private static String xpMode = "levels";
-	private static int xpPerPlayer = 68;
-	private static int orbCount = 8;
-	private static int playerRadius = 128;
-	private static boolean doGiveXP = true;
-	private static boolean doDecorationOrbs = true;
-	private static boolean doSpawnEgg = true;
-	private static boolean doDefeatAnnouncement = true;
-	private static boolean overrideEggY = false;
-	private static Vector configuredEggLocationAsVector;
+    private static int delayTicks = 80;
+    private static String xpMode = "levels";
+    private static int xpPerPlayer = 68;
+    private static int orbCount = 8;
+    private static int playerRadius = 128;
+    private static boolean doGiveXP = true;
+    private static boolean doDecorationOrbs = true;
+    private static boolean doSpawnEgg = true;
+    private static boolean doDefeatAnnouncement = true;
+    private static boolean overrideEggY = false;
+    private static Vector configuredEggLocationAsVector;
 
-	public EnderDragonDeathListener() {
-		ConfigManager configManager = Main.getConfigManager();
-		delayTicks = configManager.getInt(ConfigManager.entry_delay);
-		xpMode = configManager.getString(ConfigManager.entry_xpMode).toLowerCase();
-		xpPerPlayer = configManager.getInt(ConfigManager.entry_xpPerPlayer);
-		orbCount = configManager.getInt(ConfigManager.entry_orbCountPerPlayer);
-		doGiveXP = configManager.getBoolean(ConfigManager.entry_enableXP);
-		doDecorationOrbs = configManager.getBoolean(ConfigManager.entry_enableDecorationOrbs);
-		doSpawnEgg = configManager.getBoolean(ConfigManager.entry_enableEggRespawn);
-		doDefeatAnnouncement = configManager.getBoolean(ConfigManager.entry_enableDefeatAnnouncement);
-		overrideEggY = configManager.getBoolean(ConfigManager.entry_overrideEggY);
-		configuredEggLocationAsVector = configManager.getEggLocationAsVector();
-		playerRadius = configManager.getInt(ConfigManager.entry_playerDistanceFromOrigin);
-	}
+    public EnderDragonDeathListener() {
+        ConfigManager configManager = Main.getConfigManager();
+        delayTicks = configManager.getInt(ConfigManager.entry_delay);
+        xpMode = configManager.getString(ConfigManager.entry_xpMode).toLowerCase();
+        xpPerPlayer = configManager.getInt(ConfigManager.entry_xpPerPlayer);
+        orbCount = configManager.getInt(ConfigManager.entry_orbCountPerPlayer);
+        doGiveXP = configManager.getBoolean(ConfigManager.entry_enableXP);
+        doDecorationOrbs = configManager.getBoolean(ConfigManager.entry_enableDecorationOrbs);
+        doSpawnEgg = configManager.getBoolean(ConfigManager.entry_enableEggRespawn);
+        doDefeatAnnouncement = configManager.getBoolean(ConfigManager.entry_enableDefeatAnnouncement);
+        overrideEggY = configManager.getBoolean(ConfigManager.entry_overrideEggY);
+        configuredEggLocationAsVector = configManager.getEggLocationAsVector();
+        playerRadius = configManager.getInt(ConfigManager.entry_playerDistanceFromOrigin);
+    }
 
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onEnderDragonDeath(EntityDeathEvent e) {
-		if (!(e.getEntity() instanceof EnderDragon)) return;
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onEnderDragonDeath(EntityDeathEvent e) {
+        if (!(e.getEntity() instanceof EnderDragon)) return;
 
-		final LivingEntity dragonEntity = e.getEntity();
-		final World theEnd = dragonEntity.getWorld();
+        final LivingEntity dragonEntity = e.getEntity();
+        final World theEnd = dragonEntity.getWorld();
 
-		if (theEnd.getEnvironment() != Environment.THE_END) return;
+        if (theEnd.getEnvironment() != Environment.THE_END) return;
 
-		e.setDroppedExp(0);
+        e.setDroppedExp(0);
 
-		theEnd.spawnParticle(Particle.FALLING_OBSIDIAN_TEAR, dragonEntity.getLocation(), 1200, 2, 1, 2, 1);
+        theEnd.spawnParticle(Particle.FALLING_OBSIDIAN_TEAR, dragonEntity.getLocation(), 1200, 2, 1, 2, 1);
 
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				if (doGiveXP)
-					giveXP(dragonEntity, theEnd);
-				if (doSpawnEgg)
-					spawnEgg(theEnd);
-				if (doDefeatAnnouncement)
-					sendDefeatAnnouncement(dragonEntity.getKiller(), theEnd);
-			}
-		}.runTaskLater(Main.getPlugin(Main.class), delayTicks);
-	}
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (doGiveXP)
+                    giveXP(dragonEntity, theEnd);
+                if (doSpawnEgg)
+                    spawnEgg(theEnd);
+                if (doDefeatAnnouncement)
+                    sendDefeatAnnouncement(dragonEntity.getKiller(), theEnd);
+            }
+        }.runTaskLater(Main.getPlugin(Main.class), delayTicks);
+    }
 
-	private void giveXP(LivingEntity dragonEntity, World theEnd) {
-		// For every player in the End...
-		for (Player player : Util.getPlayersInEndCentreRadius(theEnd, playerRadius)) {
-			// Give player XP
-		    switch (xpMode) {
-		        default:
-		        case "levels":
-		            player.giveExpLevels(xpPerPlayer);
-		        case "points":
-    		        player.giveExp(xpPerPlayer);
-		    }
+    private void giveXP(LivingEntity dragonEntity, World theEnd) {
+        // For every player in the End...
+        for (Player player : Util.getPlayersInEndCentreRadius(theEnd, playerRadius)) {
+            // Give player XP
+            switch (xpMode) {
+                default:
+                case "levels":
+                    player.giveExpLevels(xpPerPlayer);
+                case "points":
+                    player.giveExp(xpPerPlayer);
+            }
 
-			Location playerLocation = player.getEyeLocation();
-			theEnd.spawnParticle(Particle.PORTAL, playerLocation, 100, 0.3f, 0.3f, 0.3f, 0.5f);
+            Location playerLocation = player.getEyeLocation();
+            theEnd.spawnParticle(Particle.PORTAL, playerLocation, 100, 0.3f, 0.3f, 0.3f, 0.5f);
 
-			if (!doDecorationOrbs) continue;
-			// Spawn XP orbs above the player (for decoration)
-			for (int i = 0; i < orbCount; i++) {
-				Location orbLocation = playerLocation.clone();
-				orbLocation.add(0, i * 4f + 3, 0);
-				theEnd.spawn(orbLocation, ExperienceOrb.class).setExperience(1);
-			}
-		}
-	}
+            if (!doDecorationOrbs) continue;
+            // Spawn XP orbs above the player (for decoration)
+            for (int i = 0; i < orbCount; i++) {
+                Location orbLocation = playerLocation.clone();
+                orbLocation.add(0, i * 4f + 3, 0);
+                theEnd.spawn(orbLocation, ExperienceOrb.class).setExperience(1);
+            }
+        }
+    }
 
-	private void spawnEgg(World theEnd) {
-		Location eggLocation = findSpawnEggLocation(theEnd);
-		if (eggLocation == null) {
-			Util.logWarning("Unable to find suitable position for Dragon Egg"
-			        + "(if this the Dragon's first death, this may be because the game has already spawned an Egg)"
-		        );
-			return;
-		}
-		eggLocation.getBlock().setType(Material.DRAGON_EGG);
-		Util.logInfo("Spawned Dragon Egg at " +
-			String.format("%s %s %s in world %s",
-				eggLocation.getBlockX(),
-				eggLocation.getBlockY(),
-				eggLocation.getBlockZ(),
-				eggLocation.getWorld().getName()
-			)
-		);
-	}
+    private void spawnEgg(World theEnd) {
+        Location eggLocation = findSpawnEggLocation(theEnd);
+        if (eggLocation == null) {
+            Util.logWarning("Unable to find suitable position for Dragon Egg"
+                    + "(if this the Dragon's first death, this may be because the game has already spawned an Egg)"
+                );
+            return;
+        }
+        eggLocation.getBlock().setType(Material.DRAGON_EGG);
+        Util.logInfo("Spawned Dragon Egg at " +
+            String.format("%s %s %s in world %s",
+                eggLocation.getBlockX(),
+                eggLocation.getBlockY(),
+                eggLocation.getBlockZ(),
+                eggLocation.getWorld().getName()
+            )
+        );
+    }
 
-	private void sendDefeatAnnouncement(Player killer, World theEnd) {
-		Bukkit.broadcastMessage(
-			Util.formatDefeatAnnouncementMessage(killer, theEnd, Main.getConfigManager())
-		);
-	}
+    private void sendDefeatAnnouncement(Player killer, World theEnd) {
+        Bukkit.broadcastMessage(
+            Util.formatDefeatAnnouncementMessage(killer, theEnd, Main.getConfigManager())
+        );
+    }
 
-	private Location findSpawnEggLocation(World theEnd) {
-		final Location searchLocation = new Location(
-	        theEnd,
-			configuredEggLocationAsVector.getBlockX(),
-			0,
-			configuredEggLocationAsVector.getBlockZ()
-		);
+    private Location findSpawnEggLocation(World theEnd) {
+        final Location searchLocation = new Location(
+            theEnd,
+            configuredEggLocationAsVector.getBlockX(),
+            0,
+            configuredEggLocationAsVector.getBlockZ()
+        );
 
-		if (overrideEggY) {
-			searchLocation.setY(configuredEggLocationAsVector.getBlockY());
-			return searchLocation;
-		}
+        if (overrideEggY) {
+            searchLocation.setY(configuredEggLocationAsVector.getBlockY());
+            return searchLocation;
+        }
 
-		final int startY = searchLocation.getBlockY();
-		final int worldHeight = theEnd.getMaxHeight();
-		// Search through the Y value of (0, 0) to find a place for the egg
-		for (int i = startY; i <= worldHeight; i++) {
-			searchLocation.setY(i);
-			final Material type = searchLocation.getBlock().getType();
-			if (type == Material.DRAGON_EGG) break;
-			if (type != Material.BEDROCK) continue;
+        final int startY = searchLocation.getBlockY();
+        final int worldHeight = theEnd.getMaxHeight();
+        // Search through the Y value of (0, 0) to find a place for the egg
+        for (int i = startY; i <= worldHeight; i++) {
+            searchLocation.setY(i);
+            final Material type = searchLocation.getBlock().getType();
+            if (type == Material.DRAGON_EGG) break;
+            if (type != Material.BEDROCK) continue;
 
-			// The block is bedrock, so...
-			Location aboveLocation = searchLocation.clone().add(0, i == worldHeight ? 0 : 1, 0);
-			// Check if the above block is air
-			if (aboveLocation.getBlock().isEmpty()) {
-				// An air block above the portal bedrock pillar has been found
-				return aboveLocation;
-			}
-		}
-		// Unable to find anywhere for the Egg to go
-		return null;
-	}
+            // The block is bedrock, so...
+            Location aboveLocation = searchLocation.clone().add(0, i == worldHeight ? 0 : 1, 0);
+            // Check if the above block is air
+            if (aboveLocation.getBlock().isEmpty()) {
+                // An air block above the portal bedrock pillar has been found
+                return aboveLocation;
+            }
+        }
+        // Unable to find anywhere for the Egg to go
+        return null;
+    }
 }
